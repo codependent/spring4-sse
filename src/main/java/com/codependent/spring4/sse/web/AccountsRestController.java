46de9db143c26ad4990ebe0e2361837889a72bb7
Jose Antonio Iñigo Garrido <jainigo@profile.es>
2016-09-20 16:15:40 +0200

    Updated

--- src/main/java/com/codependent/spring4/sse/web/AccountsRestController.java
+++ src/main/java/com/codependent/spring4/sse/web/AccountsRestController.java
@@ -2,6 +2,9 @@ package com.codependent.spring4.sse.web;
 
 import java.io.IOException;
 
+import javax.servlet.http.HttpSession;
+
+import org.apache.catalina.connector.ClientAbortException;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -12,16 +15,22 @@ import com.codependent.spring4.sse.dto.Alert;
 public class AccountsRestController {
 
 	@RequestMapping("/accounts/alerts")
-	public SseEmitter getAccountAlertsNoPathVariable() {
+	public SseEmitter getAccountAlertsNoPathVariable(HttpSession session) {
 		
 		SseEmitter emitter = new SseEmitter(Long.MAX_VALUE);
 
 		Thread t1 = new Thread(() ->{
 			try {
 				int i = 0;
-				while(++i<=3){
+				while(++i<=10000){
 					Thread.sleep(1000);
-					emitter.send(new Alert((long)i, "Alert message"+i));
+					System.out.println("Sending");
+					try{
+						emitter.send(new Alert((long)i, "Alert message"+i));
+					}catch(ClientAbortException cae){
+						cae.printStackTrace();
+						i = 10000;
+					}
 				}
 				emitter.complete();
 			} catch (IOException | InterruptedException e) {
@@ -33,4 +42,7 @@ public class AccountsRestController {
 		return emitter;
 	}
 	
+	public void sendWhileActiveSession(HttpSession session){
+		
+	}
 }
